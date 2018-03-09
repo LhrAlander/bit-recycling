07dabdc2a46f20373a11ddaa1d783621ea969210
LhrAlander <1147816814@qq.com>
2018-03-09 18:56:34 +0800

    merge

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,9 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from '@/components/Signin'
 import Verification from '@/components/Verification'
-<<<<<<< HEAD
-import Login from '@/page/login'
 import Personal from '@/page/Personal'
-=======
 import Login from '@/page/Login'
 import Index from '@/page/Index'
->>>>>>> a4e966e9f74752693e906907a424f729f8d7bb3b
 
 Vue.use(Router)
 
