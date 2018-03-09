4383277ef43bf01316a8e4045cac41f42d6f323c
LhrAlander <1147816814@qq.com>
2018-03-09 18:54:31 +0800

    完成主页

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,34 @@
+<template>
+  <footer>
+    <div class="footer-item">
+      <img src="/static/icon/indexFooterSelected.png" alt="主页">
+    </div>
+    <div class="footer-item">
+      <img src="/static/icon/orderFooter.png" alt="我要下单">
+    </div>
+    <div class="footer-item">
+      <img src="/static/icon/orderFooter.png" alt="我的信息">
+    </div>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+footer {
+  height: 58px;
+  width: 100%;
+  background-color: #fff;
+}
+
+.footer-item {
+  /* width: 25px;
+  height: 25px;
+  border: 1px solid #ddd; */
+}
+
+.footer-item img {
+  width: 25px;
+  height: 25px;
+}
+</style>
